2e6f87e106f903f07c94fa8d4d7f75e271b8d8e5
ramjirathore <ramjirathore2802@gmail.com>
2020-11-30 17:43:14 +0530

    categories reducer created

---
+++ src/store/reducers/categories.js
@@ -0,0 +1,36 @@
+import * as actionTypes from '../actions/actionTypes';
+const initialState = {
+    categories: null,
+    loading: false,
+    error: null
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+        case actionTypes.FETCH_CATEGORIES_START:
+                return {
+                    ...state,
+                    loading: true
+                };
+        case actionTypes.FETCH_CATEGORIES_SUCCESS:
+                return {
+                    ...state,
+                    loading: false
+                };
+        case actionTypes.FETCH_CATEGORIES_FAILED:
+                return {
+                    ...state,
+                    error: action.err,
+                    loading: false
+                };
+        case actionTypes.SET_CATEGORIES:
+			return {
+				...state,
+				categories: action.categories,
+            };
+		default:
+			return state;
+	}
+};
+
+export default reducer;
