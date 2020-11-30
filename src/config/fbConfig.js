70fc8c12390f21257488ec8d0755ff04627e7fe2
ramjirathore <ramjirathore2802@gmail.com>
2020-11-30 16:51:07 +0530

    firebase config added

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyByzn7AP7mu2Zb1sVb-ZVC3DPKKLOg_Pd8",
+    authDomain: "codinity-6ab53.firebaseapp.com",
+    databaseURL: "https://codinity-6ab53.firebaseio.com",
+    projectId: "codinity-6ab53",
+    storageBucket: "codinity-6ab53.appspot.com",
+    messagingSenderId: "664481511537",
+    appId: "1:664481511537:web:1177ea783560e19c0c03d0",
+    measurementId: "G-85LJX8D9P9"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampInSnapshots: true });
+  
+export default firebase;
