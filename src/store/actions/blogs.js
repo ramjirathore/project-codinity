de7431115cf5d129101939a397e73a60bd43181f
ramjirathore <ramjirathore2802@gmail.com>
2020-12-04 17:55:57 +0530

    blog action file created

---
+++ src/store/actions/blogs.js
@@ -0,0 +1,52 @@
+import * as actionTypes from '../actions/actionTypes';
+import axios from 'axios';
+
+export const fetchBlogsStart = () => {
+	return {
+		type: actionTypes.FETCH_BLOGS_START,
+	};
+};
+
+export const fetchBlogsSuccess = () => {
+	return {
+		type: actionTypes.FETCH_BLOGS_SUCCESS,
+	};
+};
+
+export const fetchBlogsFailed = (error) => {
+	return {
+		type: actionTypes.FETCH_BLOGS_FAILED,
+		err: error,
+	};
+};
+
+export const setBlogs = (blogs) => {
+	return {
+		type: actionTypes.SET_BLOGS,
+		blogs,
+	};
+};
+
+export const initBlogs = () => {
+	return (dispatch) => {
+		dispatch(fetchBlogsStart());
+		axios
+			.get('https://codinity-6ab53.firebaseio.com/blogs.json')
+			.then((response) => {
+				// let categories = [];
+				// for (let [key, value] of Object.entries(response.data)) {
+				// 	categories.push({
+				// 		key,
+				// 		videos: value,
+				// 		count: value.length,
+				// 	});
+				// }
+
+				dispatch(setBlogs(response.data));
+				dispatch(fetchBlogsSuccess());
+			})
+			.catch((error) => {
+				dispatch(fetchBlogsFailed(error));
+			});
+	};
+};
