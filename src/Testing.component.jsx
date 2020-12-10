91d58bd1f2ad3b4460d62fd4ccc7d62fc27104be
Hemant <hemant2132@gmail.com>
2020-12-10 17:54:03 +0530

    setting video id in categories db node

--- src/Testing.component.jsx
+++ src/Testing.component.jsx
@@ -10,6 +10,12 @@ export const Testing = () => {
 
     const uidArr = ['2AuiUKiYNqcmqA0gpoW5EMcVFs62', 'qUjH70lBJaVBsRklImEqgJR9qSj1'];      // list of user ids received
 
+    const extractToken = (url) => {
+        let pos = url.indexOf('token');
+        let res = url.substring(pos+6);
+        return res;
+    }
+
 	const approvingVideos = () => {
         console.log("clicked");
         const videosRef = db.ref().child('unapproved videos');
@@ -18,7 +24,7 @@ export const Testing = () => {
         {
             let uid = uidArr[index];
             const vidRef = videosRef.child(`${uid}`);
-            let video;
+            let video, videoId;
             vidRef.once('value', (snapshot) => {
                 video = snapshot.val();
 
@@ -28,12 +34,18 @@ export const Testing = () => {
                 }
             })
             .then( () => {
-                // console.log(uid, video);
-                const ref = db.ref(`categories/${video.tag}/${uid}`);
-                ref.set(video);
+                videoId = extractToken(video.url);
+                // console.log(videoId);
             })
             .then( () => {
-                vidRef.remove();
+                // console.log(uid, video);
+                const ref = db.ref(`categories/${video.tag}/${videoId}`);
+                console.log("pushing", ref.toString());
+                // ref.set(video);
+            })
+            .then( () => {
+                console.log("deleting");
+                // vidRef.remove();
             })
         }
 	};
